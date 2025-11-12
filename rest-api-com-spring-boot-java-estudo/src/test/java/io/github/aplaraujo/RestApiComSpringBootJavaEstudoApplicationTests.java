065e1a0de9520966d2872ae26773b26cc888a9b6
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-12 20:11:01 -0300

    Criação de projeto

---
+++ rest-api-com-spring-boot-java-estudo/src/test/java/io/github/aplaraujo/RestApiComSpringBootJavaEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiComSpringBootJavaEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
