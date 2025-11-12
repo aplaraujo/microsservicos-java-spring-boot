8e601a55bb3738c48ec376e46dfd0185cdd74ba3
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-12 18:27:32 -0300

    First commit

---
+++ greeting-service-estudo/src/main/java/io/github/aplaraujo/greeting_service_estudo/GreetingServiceEstudoApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo.greeting_service_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingServiceEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingServiceEstudoApplication.class, args);
+	}
+
+}
---
+++ greeting-service-estudo/src/test/java/io/github/aplaraujo/greeting_service_estudo/GreetingServiceEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo.greeting_service_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
