d874ce3e3911888256f947ceb285859bf416a2ee
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-14 08:20:48 -0300

    Recuperação da porta da instância

--- exchange-service-estudo/src/main/java/io/github/aplaraujo/controller/ExchangeController.java
+++ exchange-service-estudo/src/main/java/io/github/aplaraujo/controller/ExchangeController.java
@@ -1,7 +1,8 @@
 package io.github.aplaraujo.controller;
 
+import io.github.aplaraujo.environment.InstanceInformationService;
 import io.github.aplaraujo.model.Exchange;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,14 +13,17 @@ import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(value = "/exchange-service")
+@RequiredArgsConstructor
 public class ExchangeController {
     // http://localhost:8000/exchange-service/5/USD/BRL
+
+    private final InstanceInformationService informationService;
 
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(
             @PathVariable(name = "amount") BigDecimal amount,
             @PathVariable(name = "from") String from,
             @PathVariable(name = "to") String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
     }
 }
