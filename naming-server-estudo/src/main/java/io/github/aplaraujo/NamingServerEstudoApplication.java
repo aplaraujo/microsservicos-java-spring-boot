a77d57ebe4ccbc95d13b81d3c0ad83de6f0c6d3b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-17 09:57:18 -0300

    Inclusão de novo microsserviço

---
+++ naming-server-estudo/src/main/java/io/github/aplaraujo/NamingServerEstudoApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerEstudoApplication.class, args);
+	}
+
+}
