ecbbe7aa92500a7fa2808b7b5bd608f7308c0969
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-19 16:29:47 -0300

    Ajustes de rotas dos microsserviços no API Gateway

--- api-gateway-estudo/src/main/java/io/github/aplaraujo/config/ApiGatewayConfiguration.java
+++ api-gateway-estudo/src/main/java/io/github/aplaraujo/config/ApiGatewayConfiguration.java
@@ -1,24 +1,24 @@
-package io.github.aplaraujo.config;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApiGatewayConfiguration {
-    @Bean
-    public RouteLocator router(RouteLocatorBuilder builder) {
-        return builder.routes()
-
-                .route("book-service", p -> p.path("/book-service/**")
-                        .filters(f -> f.rewritePath("/book-service(?<segment>/?.*)", "/book-service${segment}"))
-                        .uri("lb://BOOK-SERVICE-ESTUDO"))
-
-                .route("exchange-service", p -> p.path("/exchange-service/**")
-                        .filters(f -> f.rewritePath("/exchange-service(?<segment>/?.*)", "/exchange-service${segment}"))
-                        .uri("lb://EXCHANGE-SERVICE-ESTUDO"))
-
-                .build();
-    }
-}
+//package io.github.aplaraujo.config;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ApiGatewayConfiguration {
+//    @Bean
+//    public RouteLocator router(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//
+//                .route("book-service", p -> p.path("/book-service/**")
+//                        .filters(f -> f.rewritePath("/book-service(?<segment>/?.*)", "/book-service${segment}"))
+//                        .uri("lb://BOOK-SERVICE-ESTUDO"))
+//
+//                .route("exchange-service", p -> p.path("/exchange-service/**")
+//                        .filters(f -> f.rewritePath("/exchange-service(?<segment>/?.*)", "/exchange-service${segment}"))
+//                        .uri("lb://EXCHANGE-SERVICE-ESTUDO"))
+//
+//                .build();
+//    }
+//}
